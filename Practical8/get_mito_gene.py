44e3b8b25b9f42058fadfa3a068decdd99f1ae41
sxtlxj <908085028@qq.com>
2020-04-05 17:16:37 +0800

    update practical 8

--- Practical8/get_mito_gene.py
+++ Practical8/get_mito_gene.py
@@ -6,15 +6,25 @@ Created on Fri Apr  3 14:22:46 2020
 """
 import re
 with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as xfile:
-    
-   
+     
+    seq = {}
     for line in xfile:
         if line.startswith('>'):
             line = line.rstrip()
            
             
-            name = re.findall(r'gene:(.*)gene_biotype',line)
-            print(name,length)
+            name = str(re.findall(r'gene:(.*)gene_biotype',line))
+            count = ''
+            length = 0
+            ls = []
+        else:
+            
+            count += line.replace('\n','').strip()
+            
+            length += len(count)
+            ls = [count, length]
+            seq[name] = ls
+    print(seq)
         
         
             
